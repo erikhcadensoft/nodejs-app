137de5ee65fa68a3392d933dd6ce16607afe9260
Erik Hornyak <erikhornyak@eriks-mbp-2019.lan>
2020-02-27 21:33:26 -0500

    udpate the application to test pipelines

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('*', (req,res, next) => {
-    res.send('<h1>Hello from node app!</h1>');
+    res.send('<h1>Hello from node app!</h1><h2>testing a pipeline push sucka!</h2>');
 });
 
 app.listen(4000, () => {
