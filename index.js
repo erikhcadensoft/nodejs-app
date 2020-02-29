3f0364f46296d60fb952d9823b9428faa81637d7
Erik Hornyak <erikhornyak@eriks-mbp-2019.lan>
2020-02-28 19:10:35 -0500

    create an app enhancement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('*', (req,res, next) => {
-    res.send('<h1>Hello from node app!</h1><h2>need to trigger a rebuild</h2>');
+    res.send('<h1>Hello from node app!</h1><h2>simulate an app update</h2>');
 });
 
 app.listen(4000, () => {
