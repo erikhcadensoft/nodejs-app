562e7a351ef34fc564a6656149d2986c7432f817
Erik Hornyak <erikhornyak@eriks-mbp-2019.lan>
2020-02-28 19:43:31 -0500

    update app message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('*', (req,res, next) => {
-    res.send('<h1>Hello from node app!</h1><h2>roll an update all in one</h2>');
+    res.send('<h1>Hello from node app!</h1><h2>except that you need to confirm the previous build version first, it is 24</h2>');
 });
 
 app.listen(4000, () => {
